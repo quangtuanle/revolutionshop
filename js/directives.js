4d0d775ff7535d903b7caa2584cbd6d1d6f36bb4
quangtuanle <lequangtuan270695@gmail.com>
2016-06-04 10:03:32 +0700

    Update giao diện
    Chỉnh sửa lại giao diện, phân tách directive với controller có sử dụng
    hàm bên JQuery tạo animation cho Categories

--- js/directives.js
+++ js/directives.js
@@ -11,7 +11,24 @@
     app.directive('categoriesRevolution', function(){
         return {
             restrict: 'E',
-            templateUrl: 'template/categories-revolution.html'
+            templateUrl: 'template/categories-revolution.html',
+            controller: function(){
+                <!--initiate accordion-->
+                var menu_ul = $('.menu > li > ul'),
+                    menu_a  = $('.menu > li > a');
+                menu_ul.hide();
+                menu_a.click(function(e) {
+                    e.preventDefault();
+                    if(!$(this).hasClass('active')) {
+                        menu_a.removeClass('active');
+                        menu_ul.filter(':visible').slideUp('normal');
+                        $(this).addClass('active').next().stop(true,true).slideDown('normal');
+                    } else {
+                        $(this).removeClass('active');
+                        $(this).next().stop(true,true).slideUp('normal');
+                    }
+                });
+            }
         };
     });
 
